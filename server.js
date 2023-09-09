16e186ec66b2c210b03127c9cc2f60eb92ea7d63
Sam <samblesswinedu@gmail.com>
2023-09-09 08:33:21 -0600

    Added connection to mongodb

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 //enviroment variables
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 //console.log(process.env);
 
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+  '<DATABASE_PASSWORD>',
+  process.env.DATABASE_PASSWORD,
+);
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connection successful');
+  });
 
 //START SERVER
 const port = process.env.PORT;
